d14c9e7985c99812919e9ce936d340a810602d67
koda takasi <kodatakasi@gmail.com>
2019-09-20 14:24:33 +0900

    不要なファイル削除しました

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,36 +1,25 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: [:show, :edit, :update, :destroy]
+  before_action :set_room, only: %i(show edit update destroy)
 
-  # GET /rooms
-  # GET /rooms.json
   def index
     @rooms = Room.all
-    # @stations = Station.all
   end
 
-  # GET /rooms/1
-  # GET /rooms/1.json
   def show
   end
 
-  # GET /rooms/new
   def new
     @room = Room.new
     @room.build_station
   end
 
-  # GET /rooms/1/edit
   def edit
-    
   end
 
-  # POST /rooms
-  # POST /rooms.json
   def create
     @room = Room.new(room_params)
 
     respond_to do |format|
-      # binding.pry
       if @room.save
         format.html { redirect_to @room, notice: '物件情報登録しました' }
         format.json { render :show, status: :created, location: @room }
@@ -41,8 +30,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /rooms/1
-  # PATCH/PUT /rooms/1.json
   def update
     respond_to do |format|
       if @room.update(room_params)
@@ -55,8 +42,6 @@ class RoomsController < ApplicationController
     end
   end
 
-  # DELETE /rooms/1
-  # DELETE /rooms/1.json
   def destroy
     @room.destroy
     respond_to do |format|
@@ -66,12 +51,10 @@ class RoomsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_room
     @room = Room.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def room_params
     params.require(:room).permit(:name, :price, :age, :address, :remarks, station_attributes: [:route1, :station1, :walking1, :route2, :station2, :walking2])
   end
